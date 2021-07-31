e940be8d9a7b36dcce01679ec057ddcbe08f307b
HanKwangHee <kh-han@hura.co.kr>
2021-07-31 14:25:05 +0900

    Link assimp library

--- src/pch.h
+++ src/pch.h
@@ -10,8 +10,13 @@
 #define _USING_KONAI3D      using namespace konai3d;
 #define _KONAI3D            ::konai3d
 
+/*assimp*/
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 
 #include "src/engine/pch.h"
+
 /*std headers*/
 
 /*windows headers*/
